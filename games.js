4d5716afbc5dc2c9dcb302a36f7997e613edcf6d
israniboi <ikrish61@gmail.com>
2023-12-20 17:56:01 -0500

    Move/ Delete files
    addd the config back to the index

--- games.js
+++
@@ -1,9 +0,0 @@
-<script>
-
-    const gameConfigurations = [
-        { id: "game1", name: "Game 1" },
-        { id: "game2", name: "Game 2" },
-        { id: "game3", name: "Game 3" }
-        // Add more games as needed
-    ];
-</script>
