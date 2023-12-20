09ac26690825c3132f95653399f7eea119e714c8
israniboi <ikrish61@gmail.com>
2023-12-20 17:46:14 -0500

    Move files
    Move the games list out of the html file and into games.js

---
+++ games.js
@@ -0,0 +1,8 @@
+<script>
+    const gameConfigurations = [
+        { id: "game1", name: "Game 1" },
+        { id: "game2", name: "Game 2" },
+        { id: "game3", name: "Game 3" }
+        // Add more games as needed
+    ];
+</script>
